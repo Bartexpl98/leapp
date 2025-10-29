d50495777127f7d16e02f557cc0aca53b2810d4c
Bartexpl98 <bartek.zmuda27@gmail.com>
2025-10-29 01:07:41 +0000

    changed topics to explore

--- src/app/components/ui/header.tsx
+++ src/app/components/ui/header.tsx
@@ -46,7 +46,7 @@ export default function Header() {
   const navItems = [
     { href: '/', label: 'Home' },
     { href: '/dashboard', label: 'Dashboard' },
-    { href: '/topics', label: 'Topics'},
+    { href: '/explore', label: 'Explore'},
     {href: '/about', label:'About'},
     { href: '/resources', label: 'Resources' /*For people to learn how to debate, evaluate sources perhaps?*/}
 
